ce30e11d833a841ebd7e2e01345b09ea0e7a9b50
bnulens <brechtnulens@gmail.com>
2021-02-10 15:42:15 +0100

    Home page comp - nested Chat comp

---
+++ client/src/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Chat from "../components/Chat";
+
+const Home = ({ user }) => {
+  return (
+    <>
+      <h1>The quick brown fox jumps over the lazy dog</h1>
+      <Chat />
+    </>
+  );
+};
+
+export default Home;
