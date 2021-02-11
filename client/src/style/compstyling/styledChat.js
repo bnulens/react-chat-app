ef65255cd0c710c526ea8f4a7c3a13ee5ba077e1
bnulens <brechtnulens@gmail.com>
2021-02-11 16:04:39 +0100

    Basic stylings

---
+++ client/src/style/compstyling/styledChat.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const ChatWrapper = styled.section`
+  height: 500px;
+  border: 1px solid green;
+`;
---
+++ client/src/style/pagestyling/styledHome.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HomeHeading = styled.section`
+  border: 1px solid red;
+
+  h1 {
+    font-weight: 900;
+    font-size: 42px;
+  }
+`;
