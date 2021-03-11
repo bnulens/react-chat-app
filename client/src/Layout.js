5479d404d563a872ecabaea7b0fd6f3bf4758d39
bnulens <brechtnulens@gmail.com>
2021-03-12 00:43:07 +0100

    Add Navbar comp to main Layout - max-width media queries on Container

--- client/src/Layout.js
+++ client/src/Layout.js
@@ -1,15 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 
+import Navbar from "./components/Navigation/Navbar";
+
 const Container = styled.div`
   margin: 0 auto;
-  max-width: 80vw;
+  max-width: 95%;
+
+  @media screen and (min-width: 1080px) {
+    max-width: 1080px;
+  }
 `;
 
 const Layout = ({ children }) => {
   return (
     <>
       <Container>
+        <Navbar />
         <main>{children}</main>
       </Container>
     </>
