1c8d4ae214a196a2c064c43261da8f73e2a1b1b8
bnulens <brechtnulens@gmail.com>
2021-03-12 00:43:16 +0100

    Removed excess pageStyling

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,17 +1,10 @@
 import React from "react";
 
 import Chat from "../components/Chat";
-import Time from "../components/Time";
-
-import { HomeHeading } from "../style/pagestyling/styledHome";
 
 const Home = () => {
   return (
     <>
-      <HomeHeading>
-        <h1>Welcome to your chat</h1>
-        <Time />
-      </HomeHeading>
       <Chat />
     </>
   );
